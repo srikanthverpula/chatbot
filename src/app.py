67351e3bdaab54b9033b5198110fec6f24e0fc83
Verpula Srikanth <sverpula@raytex.co.in>
2024-03-19 16:51:15 +0530

    code

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import os
+import uuid
 import openai
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 import helper
 import dummy
@@ -23,16 +24,29 @@ for message in st.session_state.chat_history:
         with st.chat_message("AI"):
             st.markdown(message.content)
 
-user_query = st.chat_input("Your message", key="user_input")
+unique_key = str(uuid.uuid4())
+user_query = st.chat_input("Your message",key=unique_key)
 chat_histories = st.session_state.chat_history
-
+print(user_query)
 if user_query is not None and user_query != "":
-    # st.session_state.chat_history.append(HumanMessage(user_query))
+    st.session_state.chat_history.append(HumanMessage(user_query))
     with st.chat_message("Human"):
         st.markdown(user_query)
     try:
         with st.spinner("Thinking..."):
             ai_response = helper.get_answer(user_query, st.session_state.chat_history)
+        with st.chat_message("AI"):
+            st.markdown(ai_response)
+            st.session_state.chat_history.append(AIMessage(ai_response))
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+else:
+    st.session_state.chat_history.append(HumanMessage("practitioner details"))
+    with st.chat_message("Human"):
+        st.markdown("practitioner details")
+    try:
+        with st.spinner("Thinking..."):
+            ai_response = helper.get_answer("practitioner details", st.session_state.chat_history)
         with st.chat_message("AI"):
             st.markdown(ai_response)
             st.session_state.chat_history.append(AIMessage(ai_response))
