db40d739ab103d82195cfb484c01bc619484acb4
Verpula Srikanth <sverpula@raytex.co.in>
2024-03-21 16:30:58 +0530

    updated openai module

--- src/app.py
+++ src/app.py
@@ -1,28 +1,27 @@
 import streamlit as st
 from dotenv import load_dotenv
-import helper
 from langchain_core.messages import HumanMessage,AIMessage
+import helper
+
 
 load_dotenv()
-st.title("Sample ChatBot")
+st.title("G1 Health")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history=[]
 for message in st.session_state.chat_history:
-    print(message)
-    print(isinstance(message,HumanMessage))
     if isinstance(message,HumanMessage):
         with st.chat_message("Human"):
             st.markdown(message.content)
     else:     
        with st.chat_message("AI"):
-            st.markdown(message.content)    
+            st.markdown(message)    
 
 user_query=st.chat_input("Your message")
 chat_histories=st.session_state.chat_history
 if user_query is not None and user_query !="":
     chat_user_query=user_query
-    st.session_state.chat_history.append(HumanMessage(chat_user_query))
+    st.session_state.chat_history.append(chat_user_query)
     
     with st.chat_message("Human"):
         st.markdown(user_query)
@@ -30,4 +29,4 @@ if user_query is not None and user_query !="":
         with st.spinner("Thinking..."):
             ai_response=helper.get_answer(user_query,st.session_state.chat_history)
             st.markdown(ai_response)    
-            st.session_state.chat_history.append(AIMessage(ai_response))
+            st.session_state.chat_history.append(ai_response)
