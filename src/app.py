5ce03fdc83e9b57aff407603b844872c0705668e
Verpula Srikanth <sverpula@raytex.co.in>
2024-03-21 13:51:11 +0530

    updated openai module

--- src/app.py
+++ src/app.py
@@ -1,37 +1,42 @@
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage,AIMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 import helper
 import dummy
 
-
 load_dotenv()
 st.title("Sample ChatBot")
 
 if "chat_history" not in st.session_state:
-    st.session_state.chat_history=[]
+    st.session_state.chat_history = []
+
 for message in st.session_state.chat_history:
-    if isinstance(message,HumanMessage):
+    if isinstance(message, HumanMessage):
         with st.chat_message("Human"):
             st.markdown(message.content)
-    else:     
-       with st.chat_message("AI"):
-            st.markdown(message.content)    
+    else:
+        with st.chat_message("AI"):
+            st.markdown(message.content)
 
-user_query=st.chat_input("Your message")
-chat_histories=st.session_state.chat_history
-if user_query is not None and user_query !="":
-    st.session_state.chat_history.append(HumanMessage(user_query))
-    
+user_query = st.chat_input("Your message")
+chat_histories = st.session_state.chat_history
+
+if user_query is not None and user_query != "":
+    st.session_state.chat_history = st.session_state.chat_history + [HumanMessage(user_query)]
+
     with st.chat_message("Human"):
         st.markdown(user_query)
     with st.chat_message("AI"):
         with st.spinner("Thinking..."):
-            #ai_response=get_response(user_query,st.session_state.chat_history)
-            ai_response=helper.get_answer(user_query,st.session_state.chat_history)
-            #ai_response=dummy.get_response(user_query,st.session_state.chat_history)
-            st.markdown(ai_response)    
-            st.session_state.chat_history.append(AIMessage(ai_response))
+            ai_response = helper.get_answer(user_query, st.session_state.chat_history)
+            st.markdown(ai_response)
+            st.session_state.chat_history = st.session_state.chat_history + [AIMessage(ai_response)]
+
+# Display chat history
+st.markdown("## Chat History")
+for i, message in enumerate(st.session_state.chat_history, start=1):
+    st.write(f"{i}. {message}")
+    print(message)
