445165046443315bcefbe0d04c69ca2d206bebd3
Verpula Srikanth <sverpula@raytex.co.in>
2024-03-21 16:43:20 +0530

    updated openai module

--- src/app.py
+++ src/app.py
@@ -30,4 +30,4 @@ if user_query is not None and user_query !="":
         with st.spinner("Thinking..."):
             ai_response=helper.get_answer(user_query,st.session_state.chat_history)
             st.markdown(ai_response)    
-            st.session_state.chat_history.append(AIMessage(ai_response))
+            st.session_state.chat_history.append(ai_response)
