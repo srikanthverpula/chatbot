637ed326a5e0108d1e353fe7e9a985be4287291a
Verpula Srikanth <sverpula@raytex.co.in>
2024-03-21 15:37:06 +0530

    updated openai module

--- src/app.py
+++ src/app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage,AIMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
 import helper
-import dummy
 
 
 load_dotenv()
@@ -24,7 +20,7 @@ for message in st.session_state.chat_history:
 user_query=st.chat_input("Your message")
 chat_histories=st.session_state.chat_history
 if user_query is not None and user_query !="":
-    #st.session_state.chat_history.append(HumanMessage(user_query))
+    st.session_state.chat_history.append(HumanMessage(user_query))
     
     with st.chat_message("Human"):
         st.markdown(user_query)
@@ -34,4 +30,4 @@ if user_query is not None and user_query !="":
             ai_response=helper.get_answer(user_query,st.session_state.chat_history)
             #ai_response=dummy.get_response(user_query,st.session_state.chat_history)
             st.markdown(ai_response)    
-            #st.session_state.chat_history.append(AIMessage(ai_response))
+            st.session_state.chat_history.append(AIMessage(ai_response))
